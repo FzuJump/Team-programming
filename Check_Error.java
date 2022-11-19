cfe5a74bfe2980f0dad78bc2155f96a84645134c
Bofishll <112791527+Bofishll@users.noreply.github.com>
2022-11-19 16:49:02 +0800

    错误判断模块

--- Check_Error.java
+++ Check_Error.java
@@ -1,6 +1,7 @@
-import java.util.*;
-public class Check_Error {
-    public static  boolean CheckError(String str){
+public int CheckError(String str){
+        if(str.length()>12){
+            return -1;
+        }
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         char Chinese_Bits[] = {'零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'};
         char Other[] = {'拾', '佰', '仟', '万', '拾', '佰', '仟', '亿', '拾', '佰', '仟'};
@@ -16,31 +17,30 @@ public class Check_Error {
             char c = str.charAt(i);
             if (map.containsKey(c)) {
                 a[1] = 1;//1判断是中文
-            } else if (Character.isDigit(c)) {
-                a[2] = 1;
-            } else a[0] = 1;
+            } else if (Character.isDigit(c)){
+               a[2] = 1;
+             } else a[0] = 1;
         }
-        if(a[0] == 1 || (a[1] == 1 && a[2] == 1))return false;
+        if(a[0] == 1 || (a[1] == 1 && a[2] == 1))return -1;
         if(a[1] == 1){
-            String ans1 = Chinese_Into_Num.ChineseConvertToNumber(str);
-            String str1 = Num_Into_Chinese.NumIntoChinese(Long.parseLong(ans1));
-            if(str1 == str){
-                return true;
+             String ans1 = ChineseConvertToNumber(str);
+             String str1 = NumIntoChinese(Long.parseLong(ans1));
+             if(str1 == str){
+                    return 1;
             }else{
-                return false;
-            }
+                    return -1;
+             }
         }
         if(a[2] == 1){//全数字
-            long num = Long.parseLong(str);
-            String str1 = Num_Into_Chinese.NumIntoChinese(num);//全数字转中文
-            String ans1 = Chinese_Into_Num.ChineseConvertToNumber(str1);
-            long num1 = Long.parseLong(ans1);
-            if(num == num1){
-                return true;
-            }else{
-                return false;
+        long num = Long.parseLong(str);
+        String str1 = NumIntoChinese(num);//全数字转中文
+        String ans1 = ChineseConvertToNumber(str1);
+        long num1 = Long.parseLong(ans1);
+        if(num == num1){
+            return 0;
+        }else{
+            return -1;
             }
         }
-        return true;
-    }
+        return -1;
 }
